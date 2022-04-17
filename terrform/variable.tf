71388b48eb23ee26d7283f55a21c65f21fd2329f
Matt Chin <49621217+matphy94@users.noreply.github.com>
2022-04-18 03:06:50 +1000

    Add files via upload

---
+++ terrform/variable.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    default     = "ap-southeast-2"
+}
+
+variable "cluster_name" {
+    default     = "quiz_cluster"
+}
+
+variable "vpc_name" {
+    default     = "quiz_vpc"
+}
+
+variable "cluster_version" {
+    default     = "1.22"
+}
